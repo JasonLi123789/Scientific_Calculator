4eceab52ea277c26897bca8dd6bb33b5bf1ed67f
E-voni <yvonneeeeee920@gmail.com>
2022-12-06 15:37:16 -0800

    update GUI

--- calculator.py
+++ calculator.py
@@ -2,3 +2,11 @@
 # Assignment: Group Project
 # Conmpleted(or last version): 12/05/2022
 
+Button1 = Button(Frame, text=1, width=5, height=4, font=30, command=lambda: clickbutton(1))
+Button1.grid(row=2, column=0)
+Button2 = Button(Frame, text=2, width=5, height=4, font=30, command=lambda: clickbutton(2))
+Button2.grid(row=2, column=1)
+Button3 = Button(Frame, text=3, width=5, height=4, font=30, command=lambda: clickbutton(3))
+Button3.grid(row=2, column=2)
+Minus = Button(Frame, text='-', width=5, height=4, font=30, command=lambda: clickbutton('-'))
+Minus.grid(row=2, column=4)
